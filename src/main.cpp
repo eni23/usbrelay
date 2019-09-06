9381d31f29c02e6889cb3c0013d893379d5b3f15
Cyrill von Wattenwyl <eni@e23.ch>
2019-09-06 23:11:08 +0200

    fix relay state output and nicer code format

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include "Arduino.h"
 #include "DigiCDC.h"
 
+
 char cdc_input;
 boolean relay_state = false;
+
 
 void setup() {
   pinMode( OUTPUT, PIN_RELAY );
@@ -12,12 +14,11 @@ void setup() {
 }
 
 
-
 void print_relay_state(){
   if (relay_state) {
-    SerialUSB.print("0");
-  } else {
     SerialUSB.print("1");
+  } else {
+    SerialUSB.print("0");
   }
 }
 
@@ -29,30 +30,28 @@ void loop() {
     // 0 = relay off
     if ( cdc_input == 48 ){
       relay_state = false;
-      SerialUSB.print("0");
       digitalWrite( PIN_RELAY, 0 );
+      SerialUSB.print("0");
     }
     // 1 = relay_state on
     if ( cdc_input == 49 ) {
       relay_state = true;
-      SerialUSB.print("1");
       digitalWrite( PIN_RELAY, 1 );
+      SerialUSB.print("1");
     }
     // 2 = relay toggle
     if ( cdc_input == 50 ) {
-      print_relay_state();
       digitalWrite( PIN_RELAY, !relay_state );
       relay_state = !relay_state;
+      print_relay_state();
     }
     // 3 = get relay state
     if ( cdc_input == 51 ) {
       print_relay_state();
     }
-
     // 9 = get identification byte, returns 'r' or byte 114
     if ( cdc_input == 57 ){
       SerialUSB.print("r");
     }
   }
-
 }
